74cbc147b2e329b46c48b6ac1319ed41c1cda56c
vera <cwisdom2016@163.com>
2025-02-16 01:30:39 +0800

    fix usb-printer.po

--- custom-packages/luci-app-usb-printer/luasrc/controller/usb_printer.lua
+++ custom-packages/luci-app-usb-printer/luasrc/controller/usb_printer.lua
@@ -21,6 +21,6 @@ function index()
 		return
 	end
 	
-	entry({"admin", "services"}, firstchild(), _("Services"), 40).dependent = false
-	entry({"admin", "services", "usb_printer"}, cbi("usb_printer"), _("USB Printer Server"), 44)
+	entry({"admin", "services"}, firstchild(), "Services", 44).dependent = false
+	entry({"admin", "services", "usb_printer"}, cbi("usb_printer"), _("USB Printer Server"), 50)
 end
