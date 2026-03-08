a15219873b1558cffe6b7324f12bb869f7f12e60
vera <cwisdom2016@163.com>
2026-03-08 17:50:20 +0800

    update

--- add-submodules.sh
+++ add-submodules.sh
@@ -1,5 +1,15 @@
-
-git submodule add https://github.com/tty228/luci-app-wechatpush.git luci-app-wechatpush && git submodule add https://github.com/jerrykuku/luci-theme-argon.git luci-theme-argon && git submodule add https://github.com/jerrykuku/luci-app-argon-config.git luci-app-argon-config &&
-git submodule add https://github.com/Openwrt-Passwall/openwrt-passwall.git openwrt-passwall && git submodule add https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git openwrt-passwall-packages && git submodule add https://github.com/Openwrt-Passwall/openwrt-passwall2.git openwrt-passwall2   &&
-git submodule add https://github.com/pymumu/luci-app-smartdns.git luci-app-smartdns && git submodule add https://github.com/sbwml/luci-app-mosdns.git luci-app-mosdns && git submodule add https://github.com/lisaac/luci-app-diskman.git luci-app-diskman   &&
-git submodule add https://github.com/sirpdboy/luci-app-parentcontrol.git luci-app-parentcontrol && git submodule add https://github.com/sirpdboy/netspeedtest.git netspeedtest && git submodule add https://github.com/sirpdboy/luci-app-partexp.git luci-app-partexp && git submodule add https://github.com/Vera2016/luci-app-nginx-manager.git luci-app-nginx-manager
+git submodule add -b master https://github.com/tty228/luci-app-wechatpush.git luci-app-wechatpush && \
+git submodule add -b master https://github.com/jerrykuku/luci-theme-argon.git luci-theme-argon && \
+git submodule add -b master https://github.com/jerrykuku/luci-app-argon-config.git luci-app-argon-config && \
+git submodule add -b main https://github.com/Openwrt-Passwall/openwrt-passwall.git openwrt-passwall && \
+git submodule add -b main https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git openwrt-passwall-packages && \
+git submodule add -b main https://github.com/Openwrt-Passwall/openwrt-passwall2.git openwrt-passwall2 && \
+git submodule add -b master https://github.com/pymumu/luci-app-smartdns.git luci-app-smartdns && \
+git submodule add -b v5 https://github.com/sbwml/luci-app-mosdns.git luci-app-mosdns && \
+git submodule add -b master https://github.com/lisaac/luci-app-diskman.git luci-app-diskman && \
+git submodule add -b main https://github.com/sirpdboy/luci-app-parentcontrol.git luci-app-parentcontrol && \
+git submodule add -b main https://github.com/sirpdboy/netspeedtest.git netspeedtest && \
+git submodule add -b main https://github.com/sirpdboy/luci-app-partexp.git luci-app-partexp && \
+git submodule add -b master https://github.com/brvphoenix/wrtbwmon.git wrtbwmon && \
+git submodule add -b master https://github.com/brvphoenix/luci-app-wrtbwmon.git luci-app-wrtbwmon && \
+git submodule add -b main https://github.com/Vera2016/luci-app-nginx-manager.git luci-app-nginx-manager
